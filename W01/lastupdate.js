697de8215d96a733d838666452d9f20e5463b51e
Shion034 <skeaty431998@gmail.com>
2021-01-09 14:52:57 +0900

    last update

--- W01/lastupdate.js
+++ W01/lastupdate.js
@@ -1,9 +1,17 @@
-let newdate=new Date();
+let monthes=[
+   "January","February","March",
+   "April","May","June","July",
+   "August","September","October",
+   "November","December",];
+let daynemes=[
+   "Monday","Tuesday","Wednesday","Thursday",
+   "Friday","Saturday", "Sunday",];
 
-   let year = lastup.getFullYear();
-   document.getElementById("year").textContent=year;
+   let d=new Date();
+   let dayNam=daynames[d.getDate()];
+   let monthName=months[d.getMonth()];
+   let year=d.getFullYear();
+   let fulldate=dayNam+", "+monthName+", "+d.getDate()+
+   ", "+year;
 
-   let lastupdate=document.lastModified;
-   document.getElementById("date").textContent=lastupdate;
-
-//document.write("Last update：" + document.lastModified);
+   document.getElementById("date").textContent=fulldate;
