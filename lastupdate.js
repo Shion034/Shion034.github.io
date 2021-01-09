ca364da2b91dfd61736156f4790fffcd5dc6390f
Shion034 <skeaty431998@gmail.com>
2021-01-09 12:41:53 +0900

    update

--- lastupdate.js
+++ lastupdate.js
@@ -1,9 +1,9 @@
-let newdate=new Date();
+/*let newdate=new Date();
 
    let year = lastup.getFullYear();
    document.getElementById("year").innerHTML=year;
 
    let lastupdate=document.lastModified;
    document.getElementById("date").innerHTML=+"Last update:"+lastupdate;
-
+*/
 document.write("Last update：" + document.lastModified);
