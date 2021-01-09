2e1c63536533ab61f01e4612732b3267747624f3
Shion034 <skeaty431998@gmail.com>
2021-01-09 12:22:45 +0900

    nwe update

--- lastupdate.js
+++ lastupdate.js
@@ -1,9 +1,6 @@
 let newdate=new Date();
 
-   var year = lastup.getYear();
-   var month = lastup.getMonth() + 1;
-   var day = lastup.getDate();
-   var hour = lastup.getHours();
-   var min = lastup.getMinutes(); 
-
-   document.getElementById("date").innerHTML;
+   let year = lastup.getFullYear();
+   document.getElementById("year").innerHTML=year;
+   let lastupdate=document.lastModified;
+   document.getElementById("date").innerHTML=lastupdate;
