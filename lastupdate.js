c91298a463acb18f07958d85136a5966ecf80e27
Shion034 <skeaty431998@gmail.com>
2021-01-09 12:12:40 +0900

    uppdate2

---
+++ lastupdate.js
@@ -0,0 +1,9 @@
+let newdate=new Date();
+
+   var year = lastup.getYear();
+   var month = lastup.getMonth() + 1;
+   var day = lastup.getDate();
+   var hour = lastup.getHours();
+   var min = lastup.getMinutes(); 
+
+   document.getElementById("date").innerHTML;
