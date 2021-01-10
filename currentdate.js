31641e9a9ec8998184c904b81bf0da9f693385b0
Shion034 <skeaty431998@gmail.com>
2021-01-10 13:43:00 +0900

    current date update

--- currentdate.js
+++ currentdate.js
@@ -1,16 +1,30 @@
 let monthes=[
-   "January","February","March",
-   "April","May","June","July",
-   "August","September","October",
-   "November","December"];
-let daynemes=[
-   "Monday","Tuesday","Wednesday","Thursday",
-   "Friday","Saturday", "Sunday"];
+   "January",
+   "February",
+   "March",
+   "April",
+   "May",
+   "June",
+   "July",
+   "August",
+   "September",
+   "October",
+   "November",
+   "December"];
 
-   let d=new Date();
-   let dayName=daynames[d.getDate()];
-   let monthName=months[d.getMonth()];
-   let year=d.getFullYear();
-   let fulldate=dayName+", "+monthName+", "+d.getDate()+", "+year;
+let daynemes=[
+   "Monday",
+   "Tuesday",
+   "Wednesday",
+   "Thursday",
+   "Friday",
+   "Saturday",
+   "Sunday"];
+
+let d=new Date();
+let dayName=daynames[d.getDate()];
+let monthName=months[d.getMonth()];
+let year=d.getFullYear();
+let fulldate=dayName+", "+monthName+", "+d.getDate()+", "+year;
 
    document.getElementById("currentdate").textContent=fulldate;
