1b1be9a8564b17d05d2dbb7134a18fc4a125e5c0
Shion034 <skeaty431998@gmail.com>
2021-02-15 12:26:32 +0900

    uproads

---
+++ lesson7/app.js
@@ -0,0 +1,38 @@
+
+
+let imagesToLoad = document.querySelectorAll("img[data-src]");
+
+const imgOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px 50px 0px"
+};
+
+
+
+const loadImages = (image) => {
+    image.setAttribute("src", image.getAttribute("data-src"));
+    image.onload = () =>{image.removeAttribute("data-src");};
+};
+
+imagesToLoad.forEach((img) => {
+    loadImages(img);
+});
+
+if ("IntersectionObserver" in window){
+    const imgobserver = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+            if(item.isIntersecting){
+                loadImages(item.target);
+                observer.unobserve(item.target);
+            }
+        });
+    });
+    imagesToLoad.forEach((img)=>{
+        imgobserver.observe(img);
+    });
+}
+else{
+    imagesToLoad.forEach((img)=>{
+        loadImages(img);
+    });
+}
