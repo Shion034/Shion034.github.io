8a4dd692f69d1303c63343eaec82ac8e836ee644
Shion034 <skeaty431998@gmail.com>
2021-02-18 10:43:19 +0900

    up

---
+++ lesson7/js/app.js
@@ -0,0 +1,38 @@
+
+function preloadImage(img){
+    const src = img.getAttribute("data-src");
+    if(!src){
+        return;
+    }
+    img.src = src;
+}
+
+//image loading from------------------------------------------------------------------------
+const images = document.querySelectorAll("[data-src]");
+
+
+//load images and comvert data-src attribute------------------------------------------------
+const loadImages = (image)=>{
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {
+        image.removeAttribute('data-src');
+    };
+}
+//loading speed and veiw point--------------------------------------------------------------
+const imgOptions ={
+    rootMargin:"50px",
+    threshold:0.5
+};
+
+const Observer = new IntersectionObserver ((entries, Observer)=>{
+ entries.forEach(entry => {
+     if (entry.isIntersecting){
+         loadImages(entry.target);
+         Observer.unobserve(entry.target);
+     }
+ });
+}, imgOptions);
+
+images.forEach((img) => {
+    Observer.observe(img);
+});
