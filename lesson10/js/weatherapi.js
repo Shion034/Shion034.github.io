c1b0535ac11b0914b51b4635e9b8bdfa6e06507f
Shion034 <skeaty431998@gmail.com>
2021-03-11 22:35:08 +0900

    js

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,7 +1,20 @@
-const apiURL = "https://samples.openweathermap.org/data/2.5/forecast?id=5604473&appid=droppyweather753159";
+const apiURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=2ba8af18e29c9d91e0f7848f6e1aebec";
 
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
-  });
+
+    document.getElementById('current-temp').textContent = jsObject.main.temp;
+  
+  // note the concatenation
+  const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png'; 
+  // note how we reference the weather array 
+  const desc = jsObject.weather[0].description;  
+   // informational specification only
+  document.getElementById('imagesrc').textContent = imagesrc; 
+  // focus on the setAttribute() method
+  document.getElementById('icon').setAttribute('src', imagesrc);  
+  document.getElementById('icon').setAttribute('alt', desc);
+
+});
