267b75ae5f1d4aad22867636d3b9fbc76f317b46
Shion034 <skeaty431998@gmail.com>
2021-03-13 17:44:13 +0900

    elements

---
+++ lesson10/js/bannerad.js
@@ -0,0 +1,17 @@
+let daynamess = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"];
+let dd= new Date();
+let dayNname = daynamess[dd.getDay()];
+
+if(dayNname=="Saturday"){
+document.getElementById("banner").innerHTML = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
+}
+else{
+    document.getElementById("banner").innerHTML = "No events for today.";
+}
