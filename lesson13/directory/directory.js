231d451aa5ac1b23566e4be2a2d2ee1677364177
Shion034 <skeaty431998@gmail.com>
2021-04-06 06:36:07 +0900

    up

--- lesson13/directory/directory.js
+++ lesson13/directory/directory.js
@@ -8,4 +8,16 @@ fetch(directs)
     const directers = jsonObject['directers'];
     console.log(directers);
 
-}
+    const part = document.querySelector(".directersTable");
+
+    part.forEach(part => {
+
+      let card = document.createElement('section');
+      let logo = document.createElement ('img');
+      let h2 = document.createElement('h2');
+      let p1 = document.createElement('p');
+      let p2 = document.createElement('p');
+      
+        
+    });
+});
